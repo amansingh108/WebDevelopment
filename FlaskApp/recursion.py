ed1bba062f79051a4f039965b4b6b3b57de7bf96
prajwolthapa <prajwolthapa@hotmail.com>
2017-11-01 23:26:47 +1100

    Recusion added factorial calulation

--- FlaskApp/recursion.py
+++ FlaskApp/recursion.py
@@ -9,3 +9,14 @@ def fibonaci(n):
 
 for n in range(1,1001):
 	print("Current Number :" , n , "Value :",fibonaci(n)) 
+
+
+
+def factorial(n):
+	if n<=1:
+		return 1
+	else:
+		return n* factorial(n-1)
+
+if __name__ == '__main__':
+	factorial()
